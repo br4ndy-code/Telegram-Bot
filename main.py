8734d81f3dcaa55b987a369d23ff0421a8fe4956
br4ndy-code <naxelord@gmail.com>
2021-11-12 14:22:11 +0200

    Add two buttons

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 import config
+from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -8,7 +9,23 @@ bot = telebot.TeleBot(config.TOKEN)
 def start(message):
     sticker = open('sticker.webp', 'rb')
     bot.send_sticker(message.chat.id, sticker)
-    bot.send_message(message.chat.id, "Welcome!")
+    # keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton('Settings')
+    btn2 = types.KeyboardButton('Help')
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, 'Welcome!', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    if message.chat.type == 'private':
+        if message.text == 'Settings':
+            bot.send_message(message.chat.id, 'There will be settings!')
+        elif message.text == 'Help':
+            bot.send_message(message.chat.id, 'This bot was developed just for fun by br4ndy-code!')
+        else:
+            bot.send.message(message.chat.id, 'Sorry, I didn\'t understand you :(')
 
 
 bot.polling(none_stop=True)
